720b4d9fc25b483688d4c5cc5b2563ec34171622
XIAOYUNYANG <yangx232@gmail.com>
2013-11-05 20:45:25 -0500

    Front End Html javascript: without game page.

---
+++ legacy/resources/assets/js/jquery_002.js
@@ -0,0 +1,1 @@
+jQuery.extend({nette:{updateSnippet:function(id,html){$("#"+id).html(html)},success:function(payload){if(payload===null)return;if(payload.redirect){window.location.href=payload.redirect;return}if(payload.snippets){for(var i in payload.snippets){jQuery.nette.updateSnippet(i,payload.snippets[i])}}}}});jQuery.ajaxSetup({success:jQuery.nette.success});
